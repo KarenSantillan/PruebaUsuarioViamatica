f01065ec2339bcb2aed5ffd20b973641eabf633d
KarenSantillan <kasasegu@espol.edu.ec>
2024-01-02 19:57:11 -0500

    Inicio

---
+++ src/main/java/com/santillan/usuariosviamatica/UsuariosViamaticaApplication.java
@@ -0,0 +1,13 @@
+package com.santillan.usuariosviamatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosViamaticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsuariosViamaticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/santillan/usuariosviamatica/UsuariosViamaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santillan.usuariosviamatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosViamaticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
